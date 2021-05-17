1edace87166a4f857f0089dc3efe20d75e40dbf7
Imaad Uddin <imaaduddin@icloud.com>
2021-05-17 18:17:41 -0500

    logo componenet

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ function App() {
   return (
     <div className="App">
       <Navigation />
-      {/* <Logo />
-      <ImageLinkForm />
+      <Logo />
+      {/* <ImageLinkForm />
       <FaceRecognition /> */}
     </div>
   );
--- src/Components/Logo/Logo.js
+++ src/Components/Logo/Logo.js
@@ -1,9 +1,12 @@
 import React from "react";
+import Tilt from 'react-tilt';
 
 const Logo = () => {
   return (
     <div className="ma4 mt0">
-
+      <Tilt className="Tilt" options={{ max : 25 }} style={{ height: 250, width: 250 }} >
+        <div className="Tilt-inner"> HI </div>
+      </Tilt>
     </div>
   )
 }
