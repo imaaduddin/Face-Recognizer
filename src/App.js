653ffce054d46510b877b244e0bb5ef139e4d344
Imaad Uddin <imaaduddin@icloud.com>
2021-05-18 16:00:25 -0500

    created and worked on other componenets

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import "./App.css";
 import Navigation from "./Components/Navigation/Navigation"
 import Logo from "./Components/Logo/Logo";
+import ImageLinkForm from "./Components/ImageLinkForm/ImageLinkForm";
 
 function App() {
   return (
     <div className="App">
       <Navigation />
       <Logo />
-      {/* <ImageLinkForm />
-      <FaceRecognition /> */}
+      <ImageLinkForm />
+      {/* <FaceRecognition /> */}
     </div>
   );
 }
--- src/Components/Logo/Logo.js
+++ src/Components/Logo/Logo.js
@@ -1,11 +1,16 @@
 import React from "react";
 import Tilt from 'react-tilt';
+import "./Logo.css";
+import brain from "./brain.png";
 
 const Logo = () => {
   return (
     <div className="ma4 mt0">
-      <Tilt className="Tilt" options={{ max : 25 }} style={{ height: 250, width: 250 }} >
-        <div className="Tilt-inner"> HI </div>
+      {/* below code from: https://www.npmjs.com/package/react-tilt */}
+      <Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 250, width: 250 }} >
+        <div className="Tilt-inner pa3"> 
+          <img style={{paddingTop: "5px"}}alt="logp" src={brain} /> 
+        </div>
       </Tilt>
     </div>
   )
