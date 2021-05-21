0632313959bad7596082c1114e334db6a59011eb
Imaad Uddin <imaaduddin@icloud.com>
2021-05-20 20:50:20 -0500

    styled a bit and added another component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import Navigation from "./Components/Navigation/Navigation"
 import Logo from "./Components/Logo/Logo";
 import ImageLinkForm from "./Components/ImageLinkForm/ImageLinkForm";
+import Rank from "./Components/Rank/Rank";
 
 function App() {
   return (
     <div className="App">
       <Navigation />
       <Logo />
+      <Rank />
       <ImageLinkForm />
       {/* <FaceRecognition /> */}
     </div>
