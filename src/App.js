eca0d02af7502e4be5fc4fffcffbb94fcadb956e
Imaad Uddin <imaaduddin@icloud.com>
2021-05-20 21:16:44 -0500

    added new component and some stlying

--- src/App.js
+++ src/App.js
@@ -3,10 +3,31 @@ import Navigation from "./Components/Navigation/Navigation"
 import Logo from "./Components/Logo/Logo";
 import ImageLinkForm from "./Components/ImageLinkForm/ImageLinkForm";
 import Rank from "./Components/Rank/Rank";
+import Particles from 'react-particles-js';
 
+// Got below code from: https://www.npmjs.com/package/react-particles-js
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 40,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    },
+    shadow: {
+      enable: true,
+      color: "#3CA9D1",
+      blur: 5
+    }
+  }
+}
 function App() {
   return (
     <div className="App">
+      <Particles className="particles"
+        params={particlesOptions} 
+      />
       <Navigation />
       <Logo />
       <Rank />
