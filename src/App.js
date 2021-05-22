44a6a7fb1f39e0397df9714f205535b8b1256315
Imaad Uddin <imaaduddin@icloud.com>
2021-05-22 13:20:31 -0500

    clicking button

--- src/App.js
+++ src/App.js
@@ -36,6 +36,10 @@ class App extends Component {
     console.log(event.target.value);
   }
 
+  onButtonSubmit = () => {
+    console.log("click");
+  }
+
   render() {
     return (
       <div className="App">
@@ -45,7 +49,7 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm onInputChange={this.onInputChange} />
+        <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
         {/* <FaceRecognition /> */}
       </div>
     );
